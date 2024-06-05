b43e7a8e82c7fa7119079e1dfe0a52d5d4243c08
Yuri <yuri.kamau@student.moringaschool.com>
2024-06-05 12:52:16 +0300

    organizing-bears-lab

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+
+
+INSERT INTO bears (name,age, sex,color, temperament, alive ) VALUES ("Mr. Chocolate",1,"M","red","text", true),
+("Rowdy",2,"F","yellow","text", true),
+("Tabitha",3,"M","green","text", true),
+("Sergeant Brown",4,"F","brown","text", true),
+("Melissa",4,"F","white","text", true),
+("Grinch",5,"","pink","text", true),
+("Wendy",6,"F","purple","text", true),
+(NULL,6,"F","purple","text", true)
+
